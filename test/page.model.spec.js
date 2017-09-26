b2d836241b65b03f3ecf9a809279b1c0c345edfd
Mark Shin <pe.markshin@gmail.com>
2017-09-26 16:37:00 -0400

    Instance & Validation Edits

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -1,7 +1,11 @@
+'use strict'
+
 const expect = require('chai').expect;
 const { Page } = require('../models');
+const chai = require("chai");
+const should = chai.should();
 
-
+chai.use(require('chai-things'));
 
 describe('Page model', function () {
     describe('Virtuals', function () {
@@ -28,7 +32,6 @@ describe('Page model', function () {
     describe('Class methods', function () {
       before(function (done) {
 
-        // delete table and recreate with our new schema
         Page.sync({force: true})
         .then(function(){
           return Page.create({
@@ -42,6 +45,7 @@ describe('Page model', function () {
         })
         .catch(done);
       });
+
       describe('findByTag', function () {
         it('gets pages with the search tag', function(done) {
           Page.findByTag('bar')
@@ -49,7 +53,7 @@ describe('Page model', function () {
               expect(pages).to.have.lengthOf(1);
               done();
             })
-            .catch(done)
+            .catch(done);
         });
         it('does not get pages without the search tag', function(done) {
           Page.findByTag('hello')
@@ -57,21 +61,63 @@ describe('Page model', function () {
               expect(pages).to.have.lengthOf(0);
               done();
             })
-            .catch(done)
+            .catch(done);
         });
       });
     });
 
     describe('Instance methods', function () {
+      let page1, page2, page3;
+      before(function () {
+        page1 = Page.build({
+          title: 'foo',
+          content: 'bar',
+          tags: ['foo','bar']
+        });
+        page2 = Page.build({
+          title: 'foo',
+          content: 'bar',
+          tags: ['foo','bass']
+        });
+        page3 = Page.build({
+          title: 'foo',
+          content: 'bar',
+          tags: ['hello']
+        });
+        // page1.save();
+        // page2.save();
+        // page3.save();
+      });   
+
       describe('findSimilar', function () {
-        it('never gets itself');
-        it('gets other pages with any common tags');
-        it('does not get other pages without any common tags');
+        it('never gets itself', function() {
+          expect(page1.findSimilar).should.not.include(page1);
+        });
+        it('gets other pages with any common tags', function(){
+
+        });
+        it('does not get other pages without any common tags', function(){
+
+        });
       });
     });
 
     describe('Validations', function () {
-      it('errors without title');
+      let page;
+      beforeEach(function(){
+        page = Page.build();
+      });
+      it('errors without title', function(done){
+        page.validate() // return a valid page
+        .then( function () { done(); })
+        .catch(function(err){
+          expect(err).to.exist;
+          expect(err.errors).to.exist;
+          expect(err.errors[0].path).to.equal('title');
+          done();
+          console.log('Error');
+        })
+      });
       it('errors without content');
       it('errors given an invalid status');
     });
